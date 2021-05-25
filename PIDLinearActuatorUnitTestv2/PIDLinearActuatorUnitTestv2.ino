8d60d7e9e5aef811790aaacac6eabb1d279967eb
BrunoHinojosa <71606590+BrunoHinojosa@users.noreply.github.com>
2021-05-25 18:08:33 -0400

    Update PIDLinearActuatorUnitTestv2.ino

--- PIDLinearActuatorUnitTestv2/PIDLinearActuatorUnitTestv2.ino
+++ PIDLinearActuatorUnitTestv2/PIDLinearActuatorUnitTestv2.ino
@@ -23,18 +23,7 @@ void setup()
   pinMode(IN2, OUTPUT);
   pinMode(IO_PIN, INPUT);
   pinMode(ANV_PIN, OUTPUT);
-<<<<<<< Updated upstream:PIDLinearActuatorUnitTestv2/PIDLinearActuatorUnitTestv2.ino
-
-  Hpid.SetMode(AUTOMATIC);
-  
-  initiate();
-  delay(1000);
-  currentLengthIO = pulseIn(IO_PIN, HIGH) / 1000 *1023 ;
-  Serial.println(currentLengthIO);
-=======
   PWMInput.begin(true); //PWM reads high duration 
->>>>>>> Stashed changes:attempt2/attempt2.ino
-
   //PID setup
   SetpointH = 500; //Setpoint has to be between 0 and 1023
   Hpid.SetOutputLimits(0,255);
@@ -43,61 +32,9 @@ void setup()
 }
 void loop() 
 {
-<<<<<<< Updated upstream:PIDLinearActuatorUnitTestv2/PIDLinearActuatorUnitTestv2.ino
-    //lenght input test
+      //lenght input test
 //  digitalWrite(IN1, 0);
 //  digitalWrite(IN2, 0);
-//  analogWrite(ANV_PIN, 127);
-//  delay(1000);
-  
-//  currentLengthIO = analogRead(IO_PIN);
-//  currentLengthIO = pulseIn(IO_PIN, LOW);
-//  Serial.print(currentLengthIO);
-//  Serial.print("---");
-//  currentLengthIO = pulseIn(IO_PIN, LOW);
-//  Serial.println(currentLengthIO);
-//Serial.println(PIND3);
-
-//Serial.println(PINC);
-
-
-  //extention test
-//  currentLengthIO = map(pulseIn(IO_PIN, LOW), 0, 1000, 0, 1023);
-//  Serial.println(currentLengthIO);
-//  switch(directionTest)
-//  {
-//    case 0:
-//      analogWrite(ANV_PIN,255);
-////      extend();
-//      Serial.println("testing testing");
-//      if(currentLengthIO > 995)
-//      {
-//        Serial.println("Switching direction");
-//        directionTest = 1;
-//      }
-//      break;
-//    case 1:
-//      analogWrite(ANV_PIN,10);
-////      retract();
-//      if(currentLengthIO < 100)
-//      {
-//        Serial.println("Switching direction");
-//        directionTest = 0;
-//      }
-//      break;
-//  }
-  //PID Test
-  Hpid.SetTunings(HP,HI,HD);
-  InputH = map(pulseIn(IO_PIN, LOW), 0, 1000, 0, 1023);
-  Hpid.Compute();
-//  OutputH = map(OutputH, 0, 255, 0, 1023);
-  
-  Serial.println(OutputH);
-//  if(InputH < 0)
-=======
-      //lenght input test
-  digitalWrite(IN1, 0);
-  digitalWrite(IN2, 0);
   analogWrite(ANV_PIN,0);
   delay(5000);
   currentLengthIO = PWMInput.getValue();
@@ -134,20 +71,19 @@ void loop()
 //    break;
 //  }
   //PID Test
-//  InputH = map(PWMInput.getValue(), 0, 1000, 0, 1023);
-//  Hpid.SetTunings(HP,HI,HD);
-//  Hpid.Compute();
-//  if(OutputH < 0)
->>>>>>> Stashed changes:attempt2/attempt2.ino
-//  {
-////    retract();
+  InputH = map(PWMInput.getValue(), 0, 1000, 0, 1023);
+  Hpid.SetTunings(HP,HI,HD);
+  Hpid.Compute();
+  if(OutputH < 0)
+  {
+    retract();
     analogWrite(ANV_PIN, abs(OutputH));
-//  }
-//  if(InputH >= 0)
-//  {
-////    extend();
-//    analogWrite(ANV_PIN, abs(OutputH)); 
-//  }
+  }
+  if(InputH >= 0)
+  {
+    extend();
+    analogWrite(ANV_PIN, abs(OutputH)); 
+  }
  
 }
 
