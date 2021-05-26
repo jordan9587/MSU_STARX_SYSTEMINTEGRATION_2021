967c423b55dba2a73084b3e0d48e22a18baad219
Jordan Hybki <hybkijor@msu.edu>
2021-05-26 03:02:01 -0400

    Update main.ino
    Updated main to be more readable with comments and recent discoveries from testing. Need to reduce memory in order to fit on MKR1010 wifi Arduino.

--- main/main.ino
+++ main/main.ino
@@ -1,109 +1,115 @@
 #include "svmPrototype.h"
 #include <PID_v1.h>
+
+/// Support Vector Machine Variables
 #define NUM_SAMPLES 90
 #define NUM_AXES 3
-#define INTERVAL 20 // Originally 90
-#define THRESHOLD 250 // NEW
+// Time interval for delaying EMG recordings.
+#define INTERVAL 20
+#define LOWERTHRESHOLD 250
 // To reduce spikes in reading we set a sensible value to truncate too large EMG voltage values.
-#define TRUNCATE 1000 // NEW
-//set the PID pins for Knee and Hip actuators
-#define PIN_INH 0
-#define PIN_OUTH 1
-#define PIN_INK 2
-#define PIN_OUTK 3
-#define KP 1200
-#define KI 25000
-#define KD 10000
-float baseline[NUM_AXES];  // NEW
+#define HIGHERTHRESHOLD 1000
 float features[NUM_SAMPLES * NUM_AXES];
 int emg0 = A0;
 int emg1 = A1;
 int emg2 = A2;
 //PID object variables
 int currentStateMovement = 0;
-double SetpointH, InputH, OutputH, SetpointK, InputK, OutputK;
-//Movement label values for Hip angle, Knee angle, Hip Setpoint, Knee Setpoint respectively
-float Standing[] = { 0, 0, 18.25, 21.21 };      //1
-float HeelStrike[] = { 20, 0, 16.22, 21.21 };   //2
-float LdgResp[] = { 20, 20, 19.13, 20.160 };    //3
-float MidStance[] = { 0, 5, 18.25, 20.98 };     //4
-float TmlStance[] = { -20, 0, 17.21, 21.21 };   //5
-float PreSwing[] = { -10, 40, 17.74, 18.85 };   //6
-float InitSwing[] = { 15, 60, 18.93, 17.39 };   //7
-float MidSwing[] = { 25, 25, 19.31, 19.86 };    //8
-float TmlSwing[] = { 20, 0, 19.13, 21.21 };     //9
-int arSize = sizeof(Standing) / sizeof(Standing[0]);
 Eloquent::ML::Port::SVM clf;
-PID Hpid(&InputH, &OutputH, &SetpointH, HipP, HipI, HipD, DIRECT);
-PID Kpid(&InputK, &OutputK, &SetpointK, KneeP, KneeI, KneeD, DIRECT);
+
+
+/// PID Variables/ Linear Actuator
+#define PIN_INH 0
+#define PIN_OUTH 1
+#define PIN_INK 2
+#define PIN_OUTK 3
+
+#define KP 1200
+#define KI 25000
+#define KD 10000
+double SetpointH, InputH, OutputH, SetpointK, InputK, OutputK;
+// Array of Movement label values for Hip angle, Knee angle, Hip Setpoint, Knee Setpoint respectively.
+float Standing[] = { 0, 0, 18.25, 21.21 };      // State 1
+float HeelStrike[] = { 20, 0, 16.22, 21.21 };   // State 2
+float LdgResp[] = { 20, 20, 19.13, 20.160 };    // State 3
+float MidStance[] = { 0, 5, 18.25, 20.98 };     // State 4
+float TmlStance[] = { -20, 0, 17.21, 21.21 };   // State 5
+float PreSwing[] = { -10, 40, 17.74, 18.85 };   // State 6
+float InitSwing[] = { 15, 60, 18.93, 17.39 };   // State 7
+float MidSwing[] = { 25, 25, 19.31, 19.86 };    // State 8
+float TmlSwing[] = { 20, 0, 19.13, 21.21 };     // State 9
+int arSize = sizeof(Standing) / sizeof(Standing[0]);  // Size of array
+PID pidH(&InputH, &OutputH, &SetpointH, KP, KI, KD, DIRECT);
+
+/*
+* Setup code that runs once before loop execution function.
+*/
+void setup() {
+  Serial.begin(115200);
+  SetpointH = Standing[2];
+  pidH.SetOutputLimits(0,255);
+  pidH.SetTunings(KP, KI, KD);
+}
+
+/*
+* Loop code that runs repeatedly on every tick of the microprocessor clock cycle.
+*/
+void loop() {
+  float v0, v1, v2;
+
+  // Read EMG sensors
+  readEMG(v0, v1, v2);
+  
+  // Condition/ make any normalisation to the 3 emg sensor input.
+  // Calibration procedure to remove this fixed offset from the readings. All EMG Voltage lines will oscillate from LOWER THRESHOLD (255) - HIGHER THRESHOLD (1000).
+  v0 = constrain(v0, 0, HIGHERTHRESHOLD);
+  v1 = constrain(v1, 0, HIGHERTHRESHOLD);
+  v2 = constrain(v2, 0, HIGHERTHRESHOLD);
+  while (motionDetected(v0, v1, v2) == false)
+  {
+    // Wait until motion has been detected. Motion should be between 
+  }
+
+  // Record emg data into svm format [v0, v1, v2, v0, v1, v2, ... , NUM_SAMPLES'th]
+  recordEMG();
+
+  // Use SVM to classify emg dataset
+  svmClassify();
+
+  FSM(currentStateMovement);
+}
+
+/*
+* SVM Function - Analog reads the three EMG data per leg.
+* @param v0 float of EMG sensor 1.
+* @param v1 float of EMG sensor 2.
+* @param v2 float of EMG sensor 3.
+*/
 void readEMG(float &v0, float &v1,float &v2) {
-  // Read each emg sensor and convert to voltage (0-5V)
-  // Should be recorded as analogread * (5.0 / 1023.0) in final version.
+  // Read each emg sensor and convert to analog signal (0-1023).
   v0 = analogRead(emg0);
   v1 = analogRead(emg1);
   v2 = analogRead(emg2);
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  calibrate();
-  SetpointH = Standing[2];
-  Hpid.SetOutputLimits(0,255);
-  Kpid.SetOutputLimits(0,255);
-  Hpid.SetTunings(KP, KI, KD);
-  Kpid.SetTunings(KD, KI, KD);
-}
-
-void loop() {
-  float v0, v1, v2;
-  // put your main code here, to run repeatedly:
-  if (motionDetected(v0, v1, v2) == true) {
-    // Read EMG sensors
-    readEMG(v0, v1, v2);
-  
-    // Condition/ make any normalisation to the emg sensor input.
-    // Calibration procedure to remove this fixed offset from the readings. All EMG Voltage lines will oscillate from 0.
-    v0 = constrain(v0 - baseline[0], -TRUNCATE, TRUNCATE);
-    v1 = constrain(v1 - baseline[1], -TRUNCATE, TRUNCATE);
-    v2 = constrain(v2 - baseline[2], -TRUNCATE, TRUNCATE);
-  
-    // Record emg data into svm format [v0, v1, v2, v0, v1, v2, ... , NUM_SAMPLESth v3]
-    recordEMG();
-    // Use SVM to classify emg dataset
-    svmClassify();
-    FSM(currentStateMovement);
-  }
-  else {
-    // No movement detected, return 0 as voltage.
-    v0 = 0;
-    v1 = 0;
-    v2 = 0;
-  }
-
-}
-
-//NEW
-// "Zero" the readings by setting baseline startup noise
-void calibrate() {
-    float v0, v1, v2;
-
-    for (int i = 0; i < 10; i++) {
-        readEMG(v0, v1, v2);
-        delay(10);
-    }
-
-    baseline[0] = v0;
-    baseline[1] = v1;
-    baseline[2] = v2;
-}
-
-// NEW
+/*
+* SVM Function - Check if the EMG value 
+*/
 bool motionDetected(float v0, float v1, float v2) 
 {
-  return (abs(v0) + abs(v1) + abs(v2)) > THRESHOLD;
+  if (abs(v0) <= LOWERTHRESHOLD || abs(v1) <= LOWERTHRESHOLD || abs(v2) <= LOWERTHRESHOLD)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
 }
 
+/*
+* SVM Function - Record EMG data of 3 sensors to features array. Constrains values between Lower and Higher Threshold.
+*/
 void recordEMG() {
     // Record emg data into svm format [v0, v1, v2, v0, v1, v2, ... , NUM_SAMPLESth v3]
     float v0, v1, v2;
@@ -111,9 +117,9 @@ void recordEMG() {
     {
         readEMG(v0, v1, v2);
         // Calibration procedure to remove this fixed offset from the readings. All EMG Voltage lines will oscillate from 0.
-        v0 = constrain(v0 - baseline[0], -TRUNCATE, TRUNCATE);
-        v1 = constrain(v1 - baseline[1], -TRUNCATE, TRUNCATE);
-        v2 = constrain(v2 - baseline[2], -TRUNCATE, TRUNCATE);
+        v0 = constrain(v0, LOWERTHRESHOLD, HIGHERTHRESHOLD);
+        v1 = constrain(v1, LOWERTHRESHOLD, HIGHERTHRESHOLD);
+        v2 = constrain(v2, LOWERTHRESHOLD, HIGHERTHRESHOLD);
         
         features[i * NUM_AXES + 0] = v0;
         features[i * NUM_AXES + 1] = v1;
@@ -121,73 +127,75 @@ void recordEMG() {
         delay(INTERVAL);
     }
 }
+
+/*
+* SVM Function - Classify current movement label.
+*/
 void svmClassify() {
     // Use SVM to predict label for given emg input array.
     Serial.print("Detected gesture: ");
-    currentStateMovement = clf.predictLabel(features);
     
-    Serial.println(currentState);
+    currentStateMovement = String(clf.predictLabel(features)).toInt();
+    
+    Serial.println(currentStateMovement);
 }
+
+/*
+* Linear Actuator Function - Finite State Machine that changes position and speed of linear actuator depending on PID controller.
+*/
 void FSM(int currentStateMovement)
 {
   InputH = analogRead(PIN_INH);
-  InputK = analogRead(PIN_INK);
-  switch (index)
+  switch (currentStateMovement)
   {
     case 0:           //Standing
       SetpointH = Standing[2];
-      SetpointK = Standing[3];
       PIDCompute();
-      //get EMG data and find movement label and change the value of Movement acordingly in order to go to next state
+      //get EMG data and find movement label and change the value of Movement accordinly in order to go to next state
       break;
     case 1:
       SetpointH = HeelStrike[2];
-      SetpointK = HeelStrike[3];
       PIDCompute();
       break;
     case 2:           //LdgResp
       SetpointH = LdgResp[2];
-      SetpointK = LdgResp[3];
       PIDCompute();
       break;
     case 3:           //MidStance
       SetpointH = MidStance[2];
-      SetpointK = MidStance[3];
       PIDCompute();
       break;
     case 4:           //TmlStance
       SetpointH = TmlStance[2];
-      SetpointK = TmlStance[3];
       PIDCompute();
       break;
     case 5:           //PreSwing
       SetpointH = PreSwing[2];
-      SetpointK = PreSwing[3];
       PIDCompute();
       break;
     case 6:           //InitSwing
       SetpointH = InitSwing[2];
-      SetpointK = InitSwing[3];
       PIDCompute();
       break;
     case 7:           //MidSwing
       SetpointH = MidSwing[2];
-      SetpointK = MidSwing[3];
       PIDCompute();
       break;
     case 8:           //TmlSwing
       SetpointH = TmlSwing[2];
-      SetpointK = TmlSwing[3];
       PIDCompute();
       break;
      default:
       PIDCompute();
     }
 }
+
+/*
+* Linear Actuator Function - Compute PID values.
+*/
 void PIDCompute()
 {
-    Hpid.Compute();
-    Kpid.Compute();
+    pidH.Compute();
     analogWrite(PIN_OUTH, OutputH);
     analogWrite(PIN_OUTK, OutputK);
 }
