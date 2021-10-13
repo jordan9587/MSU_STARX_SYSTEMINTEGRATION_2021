68e427e04c8ce770ee978818fef831772ca216b2
Jordan Hybki <hybkijor@msu.edu>
2021-10-13 10:20:19 -0400

    Update Client and Server script
    Used Arduino Low Power library and RTCZero to automatically managed mode, where the WiFi NINA Module reduces its power drain bringing the overall power consumption to 30mA. Any incoming data is received and the device sends out regularly the beacon signal each 100ms to keep the AP connection alive.
    
    Fixed bug where serial connection is required for running client and server script. Now battery should work in all scenarios without computer to cord configuration.

--- Wireless Communication Tests/AP Network Test/Client/Client.ino
+++ Wireless Communication Tests/AP Network Test/Client/Client.ino
@@ -9,27 +9,29 @@
 
 #include <SPI.h>
 #include <WiFiNINA.h>
+#include <ArduinoLowPower.h>
+#include <RTCZero.h>
 
 //#include "emgToolbox.h"
 //#include "login_credentials.h"
 
-///////please enter your sensitive data in the Secret tab/login_credentials.h
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;            // your network key index number (needed only for WEP)
+/* Please enter your sensitive data in the Secret tab/login_credentials.h */
+char ssid[] = "";        // Your network SSID (name)
+char pass[] = "";    // Your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;            // Your network key index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 
 // Initialize the WiFi client library
 WiFiClient client;
 
-// server address:
+// Server address:
 //char server[] = "http://192.168.4.1/";
 byte server[] = {192, 168, 4, 1};
 //IPAddress server(192,168,0,1);
 
-unsigned long lastConnectionTime = 0;            // last time you connected to the server, in milliseconds
-const unsigned long postingInterval = 10L; // delay between updates, in milliseconds
+unsigned long lastConnectionTime = 0;   // Last time you connected to the server, in milliseconds
+const unsigned long postingInterval = 10L;    // Delay between updates, in milliseconds
 
 /* User Input */
 int numberLine = 0;
@@ -44,78 +46,89 @@ boolean currentLineIsBlank = true;
 // Buffer of EMG array
 double emgArray[99];
 
-void setup() {
-  //Initialize serial and wait for port to open:
+void setup() 
+{
+  // Initialize serial and wait for port to open:
   Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
 
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
+  // Check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) 
+  {
     Serial.println("Communication with WiFi module failed!");
-    // don't continue
+    // Don't continue
     while (true);
   }
 
   String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) 
+  {
     Serial.println("Please upgrade the firmware");
   }
 
-  // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
+  // Attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) 
+  {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
-
-    // wait 5 seconds for connection:
+    WiFi.lowPowerMode();  // Enable WiFi Low Power Mode
+    // Wait 5 seconds for connection:
     delay(5000);
   }
-  // you're connected now, so print out the status:
+  // You're connected now, so print out the status:
   printWifiStatus();
 }
 
-void loop() {
-  // if there's incoming data from the net connection.
-  // send it out the serial port.  This is for debugging
-  // purposes only:
-  // an HTTP request ends with a blank line
+void loop() 
+{
+  /*
+   If there's incoming data from the net connection.
+   send it out the serial port.  This is for debugging
+   purposes only:
+   an HTTP request ends with a blank line
+  */
   currentLineIsBlank = true;
-  while (client.available()) {
+  while (client.available()) 
+  {
     c = client.read();
     Serial.write(c);
   }
 
-  // if ten milliseconds have passed since your last connection,
-  // then connect again and send data:
-  if (millis() - lastConnectionTime > postingInterval) {
+  /*
+   If ten milliseconds have passed since your last connection,
+   then connect again and send data:
+  */
+  if (millis() - lastConnectionTime > postingInterval) 
+  {
     httpRequest();
   }
 
 }
 
-// this method makes a HTTP connection to the server:
-void httpRequest() {
-  // close any connection before send a new request.
+// This method makes a HTTP connection to the server:
+void httpRequest() 
+{
+  // Close any connection before send a new request.
   // This will free the socket on the NINA module
   client.stop();
   //Serial.println("connecting...");
-  // if there's a successful connection:
-  if (client.connect(server, 80)) {
-    // send a standard HTTP response header
+  // If there's a successful connection:
+  if (client.connect(server, 80)) 
+  {
+    // Send a standard HTTP response header
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
-    client.println("Connection: close");  // the connection will be closed after completion of the response
-    client.println("Refresh: 1");  // refresh the page automatically every 1 sec
+    client.println("Connection: close");    // The connection will be closed after completion of the response
+    client.println("Refresh: 1");   // Refresh the page automatically every 1 sec
     //client.println();
     client.println("<!DOCTYPE HTML>");
     client.println("<html>");
     
-    // output the value of each analog input pin
+    // Output the value of each analog input pin
     int sensorValue0 = analogRead(A0);
     client.println(sensorValue0);
+    Serial.println(sensorValue0);
     // emgSensorRead(A0);
     /*
     for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
@@ -129,17 +142,20 @@ void httpRequest() {
      */
      
      client.println("</html>");
-     // note the time that the connection was made:
+     // Note the time that the connection was made:
      lastConnectionTime = millis();
-  } else {
-    // if you couldn't make a connection:
+  } 
+  else 
+  {
+    // If you couldn't make a connection:
     Serial.println("connection failed");
   }
 }
 
 void emgSensorRead(int pin0)
 {
-  if (userInputMessageCounter == 0) {
+  if (userInputMessageCounter == 0) 
+  {
       Serial.println("Please submit anything to console in order to write new emg values.");
       userInputMessageCounter += 1;
   }
@@ -195,17 +211,18 @@ void emgFeatureExtraction()
 }
 */
 
-void printWifiStatus() {
-  // print the SSID of the network you're attached to:
+void printWifiStatus() 
+{
+  // Print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your board's IP address:
+  // Print your board's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 
-  // print the received signal strength:
+  // Print the received signal strength:
   long rssi = WiFi.RSSI();
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
--- Wireless Communication Tests/AP Network Test/Server/Server.ino
+++ Wireless Communication Tests/AP Network Test/Server/Server.ino
@@ -12,90 +12,102 @@
 
 #include <SPI.h>
 #include <WiFiNINA.h>
+#include <ArduinoLowPower.h>
+#include <RTCZero.h>
 
-#include "login_credentials.h"
+//#include "login_credentials.h"
 
-///////please enter your sensitive data in the Secret tab/login_credentials.h
-char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;                 // your network key index number (needed only for WEP)
+/* Please enter your sensitive data in the Secret tab/login_credentials.h */
+char ssid[] = "";        // Your network SSID (name)
+char pass[] = "";    // Your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                 // Your network key index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 
 WiFiServer server(80);
 
-void setup() {
-  //Initialize serial and wait for port to open:
+void setup() 
+{
+  // Initialize serial and wait for port to open:
   Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
 
   Serial.println("Access Point Web Server");
 
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
+  // Check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) 
+  {
     Serial.println("Communication with WiFi module failed!");
-    // don't continue
+    // Don't continue
     while (true);
   }
 
   String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) 
+  {
     Serial.println("Please upgrade the firmware");
   }
 
-  // by default the local IP address will be 192.168.4.1
-  // you can override it with the following: E.g.
-  // WiFi.config(IPAddress(10, 0, 0, 1));
+  /*
+   By default the local IP address will be 192.168.4.1
+   you can override it with the following: E.g.
+   WiFi.config(IPAddress(10, 0, 0, 1));
+  */
 
-  // print the network name (SSID);
+  // Print the network name (SSID);
   Serial.print("Creating access point named: ");
   Serial.println(ssid);
 
   // Create open network. Change this line if you want to create an WEP network:
   status = WiFi.beginAP(ssid, pass);
-  if (status != WL_AP_LISTENING) {
+  WiFi.lowPowerMode();  // Enable WiFi Low Power Mode
+  if (status != WL_AP_LISTENING) 
+  {
     Serial.println("Creating access point failed");
-    // don't continue
+    // Don't continue
     while (true);
   }
 
-  // wait 5 seconds for connection:
+  // Wait 5 seconds for connection:
   delay(5000);
 
-  // start the web server on port 80
+  // Start the web server on port 80
   server.begin();
 
-  // you're connected now, so print out the status
+  // You're connected now, so print out the status
   printWifiStatus();
 }
 
 
 void loop() {
-  // compare the previous status to the current status
-  if (status != WiFi.status()) {
-    // it has changed update the variable
+  // Compare the previous status to the current status
+  if (status != WiFi.status()) 
+  {
+    // It has changed update the variable
     status = WiFi.status();
 
-    if (status == WL_AP_CONNECTED) {
-      // a device has connected to the AP
+    if (status == WL_AP_CONNECTED) 
+    {
+      // A device has connected to the AP
       Serial.println("Device connected to AP");
-    } else {
-      // a device has disconnected from the AP, and we are back in listening mode
+    } else 
+    {
+      // A device has disconnected from the AP, and we are back in listening mode
       Serial.println("Device disconnected from AP");
     }
   }
   
-  // listen for incoming clients
+  // Listen for incoming clients
   WiFiClient client = server.available();
-  if (client) {
+  if (client) 
+  {
     //Serial.println("new client");
-
-    while (client.connected()) {
-      if (client.available()) {
+    while (client.connected()) 
+    {
+      if (client.available()) 
+      {
         char c = client.read();
-        if (isDigit(c)) {
+        if (isDigit(c)) 
+        {
           Serial.write(c);
         }
         else {
@@ -103,27 +115,28 @@ void loop() {
         
       }
     }
-    // give the web browser time to receive the data
+    // Give the web browser time to receive the data
     delay(1);
 
-    // close the connection:
+    // Close the connection
     client.stop();
     //Serial.println("client disconnected");
   }
 }
 
 
-void printWifiStatus() {
-  // print the SSID of the network you're attached to:
+void printWifiStatus() 
+{
+  // Print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your WiFi shield's IP address:
+  // Print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 
-  // print where to go in a browser:
+  // Print where to go in a browser:
   Serial.print("To see this page in action, open a browser to http://");
   Serial.println(ip);
 }
