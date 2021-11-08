0c3422a0926b6c0250f6506e45f6247e10b0c164
BrunoHinojosa <71606590+BrunoHinojosa@users.noreply.github.com>
2021-11-07 21:19:13 -0500

    Update LinearActuatorControlSystem.ino

--- LinearActuatorControlSystem/LinearActuatorControlSystem.ino
+++ LinearActuatorControlSystem/LinearActuatorControlSystem.ino
@@ -1,28 +1,30 @@
+//PWM reading variables
 volatile unsigned long timer[2]; //timer that holds the rising and falling edge of PIN 24/DIO1->reads position waveform
 volatile double PWMin; //holds Duty cycle value
 volatile long Duty;
-double Setpoint = 2;
 double error;
-byte IN1 = 26;
-byte IN2 = 27;
-byte ANV = A0;
+//pinout constants
+const byte ANV = 2;
+const byte DIO = 24;
+const byte IN1 = 26;
+const byte IN2 = 27;
+
 void setup() 
 {
   Serial.begin(9600);
-  pinMode(24, INPUT);
+  pinMode(DIO, INPUT);
   pinMode(IN1,OUTPUT);
   pinMode(IN2,OUTPUT);
   pinMode(ANV,OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(24),ISR1, CHANGE); //attaches interrupt service routine to PIN 24 and triggers anytime there is a change in input
-  pinMode(2,OUTPUT);
-  digitalWrite(A0,HIGH);
+  attachInterrupt(digitalPinToInterrupt(24),ISR1, CHANGE); //attaches interrupt service routine to PIN 24 and triggers anytime there is a change in input  
+  analogWrite(ANV,255);
+  digitalWrite(IN1,HIGH);
+  //digitalWrite(IN2,HIGH);
 }
 void loop() 
 {
-  
-//  Serial.println(error);
-//  error = Setpoint - PWMin;
-//  Mdirection(error);
+  //PIDCompute(10,50,60);
+  Serial.println((PWMin));
 }
 void ISR1() 
 {
@@ -34,7 +36,7 @@ void ISR1()
     case 1:
     timer[1] = micros();
     Duty = timer[1] - timer[0];
-    PWMin = Duty*7.75/1000;
+    PWMin = Duty - 502;
     break;
   }
 }
